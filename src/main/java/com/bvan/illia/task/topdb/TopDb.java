79ae597eaa251a66f155325f225dcfc7cbaf5311
Illia Zybin <illia.zybin@globallogic.com>
2020-07-03 09:46:49 +0300

    try to do task by Queue tests are not updated

--- src/main/java/com/bvan/illia/task/topdb/TopDb.java
+++ src/main/java/com/bvan/illia/task/topdb/TopDb.java
@@ -1,10 +1,13 @@
 package com.bvan.illia.task.topdb;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.HashMap;
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
+import java.util.Map.Entry;
+import java.util.PriorityQueue;
+import java.util.Queue;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 
@@ -24,23 +27,66 @@ public class TopDb {
     private Map<String, Integer> mapOfWords = new HashMap<>();
 
     public void add(List<String> words) {
-//        for (String word : words) {
-//            Integer count = mapOfWords.get(word);
-//            mapOfWords.put(word, (count == null) ? 1 : count + 1);
-//        }
+        for (String word : words) {
+            Integer count = mapOfWords.get(word);
+            mapOfWords.put(word, (count == null) ? 1 : count + 1);
+        }
 
-        mapOfWords = words.stream()
-                .collect(Collectors.toMap(String::toString, string -> 1, Integer::sum));
     }
 
-    public Map<String, Integer> top(Map<String, Integer> map) {
-        return map.entrySet()
-                .stream()
-                .sorted((Map.Entry.<String, Integer>comparingByValue().reversed()).thenComparing(Map.Entry.<String, Integer>comparingByKey().reversed()))
-                .limit(100)
-                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (oldValue, newValue) -> oldValue,
-                        LinkedHashMap::new));
+//    public Set<Entry<String, Integer>> top() {
+//
+//        List<Entry<String, Integer>> list = new LinkedList<>(mapOfWords.entrySet());
+//
+//        Set<Entry<String, Integer>> s = new TreeSet<>(new ValueThenKeyComparator());
+//
+//        s.addAll(mapOfWords.entrySet());
+//
+//        return s;
+//    }
+//}
+
+    public List<Entry<String, Integer>> top() {
+
+        List<Entry<String, Integer>> list = new ArrayList<>();
+
+        Queue<Entry<String, Integer>> queue = new PriorityQueue<Entry<String, Integer>>(new ValueThenKeyComparator());
+
+        queue.addAll(mapOfWords.entrySet());
+
+        for (int i = 0; i < 100 ; i++) {
+            if (!queue.isEmpty()) {
+                list.add(queue.poll());
+            }
+        }
+        return list;
     }
 }
+
+
+class ValueThenKeyComparator<K extends Comparable<? super K>,
+            V extends Comparable<? super V>>
+            implements Comparator<Map.Entry<K, V>> {
+
+        public int compare(Map.Entry<K, V> a, Map.Entry<K, V> b) {
+            int cmp1 = a.getValue().compareTo(b.getValue());
+            if (cmp1 != 0) {
+                return cmp1;
+            } else {
+                return a.getKey().compareTo(b.getKey());
+            }
+        }
+
+    }
+
+    // nlogn
+//    public Map<String, Integer> top(Map<String, Integer> map) {
+//        return map.entrySet()
+//                .stream()
+//                .sorted((Map.Entry.<String, Integer>comparingByValue().reversed()).thenComparing(Map.Entry.<String, Integer>comparingByKey().reversed()))
+//                .limit(100)
+//                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (oldValue, newValue) -> oldValue,
+//                        LinkedHashMap::new));
+//    }
 
 
